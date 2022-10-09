571e27095a4fb6efad983e3abef0195923095be7
Paulo901 <paulo.m.oliveira022@gmail.com>
2022-10-09 19:02:59 -0300

    atribuido valores struct: usando ponteiros, métodos e referencia

--- 2 - Sistema de Banco/main.go
+++ 2 - Sistema de Banco/main.go
@@ -6,13 +6,19 @@ type ContaCorrente struct {
 	titular    string
 	numAgencia int
 	numConta   int
-	saldo      float32
+	saldo      float64
 }
 
 func main() {
 
 	minhaConta := ContaCorrente{"Paulo", 123456, 321, 2000}
 
+	conta2 := new(ContaCorrente)
+	conta2.titular = "Rodrigo"
+	conta2.numAgencia = 123321
+	conta2.saldo = 99
+
+	fmt.Println(*conta2)
 	fmt.Println("Dados da conta: ", minhaConta)
 
 }
