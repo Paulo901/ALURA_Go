9d34eac99b9d161a08cb18b582811f056eb3940c
Paulo901 <paulo.m.oliveira022@gmail.com>
2022-10-10 20:08:02 -0300

    OO em Go : Func de parametros ilimitados / métodos para structs

--- 2 - Sistema de Banco/main.go
+++ 2 - Sistema de Banco/main.go
@@ -9,16 +9,50 @@ type ContaCorrente struct {
 	saldo      float64
 }
 
+func (c *ContaCorrente) Saque(quantia float64) (string, float64) {
+
+	if quantia > 0 && quantia <= c.saldo {
+		c.saldo -= quantia
+		return "Saque realizado! Seu saldo : \n", c.saldo
+	}
+	return "valor indisponivel. Seu saldo : \n", c.saldo
+}
+func (c *ContaCorrente) Deposito(quantia float64) (string, float64) {
+
+	if quantia > 0 {
+		c.saldo += quantia
+		return "Deposito realizado! Seu saldo : \n", c.saldo
+	}
+	return "valor indisponivel. Seu saldo : \n", c.saldo
+
+}
+
+func (c *ContaCorrente) Transferencia(quantia float64, contaDestino *ContaCorrente) (string, float64) {
+
+	if quantia <= c.saldo {
+		c.Saque(quantia)
+		contaDestino.Deposito(quantia)
+		return "Transferencia Concluida, seu saldo: \n", c.saldo
+	}
+	return "algo deu errado, verifique seu saldo: ", c.saldo
+}
+
 func main() {
 
-	minhaConta := ContaCorrente{"Paulo", 123456, 321, 2000}
+	conta2 := ContaCorrente{"Paulo", 111, 222, 20}
+	minhaConta := ContaCorrente{}
+	minhaConta.titular = "Rodrigo"
+	minhaConta.numAgencia = 123321
+	minhaConta.saldo = 10
 
-	conta2 := new(ContaCorrente)
-	conta2.titular = "Rodrigo"
-	conta2.numAgencia = 123321
-	conta2.saldo = 99
+	result, valorNaConta := conta2.Transferencia(10, &minhaConta)
+	fmt.Println(result, valorNaConta)
+	fmt.Println(conta2.saldo, " ----", minhaConta.saldo)
 
-	fmt.Println(*conta2)
-	fmt.Println("Dados da conta: ", minhaConta)
+	testes := teste(1, 22, 33, 4, 256)
+	fmt.Println(testes)
+}
 
+func teste(num ...int) []int {
+	return num
 }
