9b21abbc0796658ba6da3faafe36e98758f817ae
Paulo901 <paulo.m.oliveira022@gmail.com>
2022-10-12 20:59:42 -0300

    corrigindo conexão localhost

--- 3 - Aplicação WEB/main.go
+++ 3 - Aplicação WEB/main.go
@@ -5,16 +5,20 @@ import (
 	"net/http"
 )
 
+type Produto struct {
+	Nome       string
+	Preco      float64
+	Quantidade int
+}
+
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
 	http.HandleFunc("/", index)
-	http.ListenAndServe(":9000", nil)
-
+	http.ListenAndServe(":5040", nil)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-
-	temp.ExecuteTemplate(w, "Index", nil)
-
+	produtos := []Produto{{"Sapato", 200, 20}, {"camisa", 50, 30}}
+	temp.ExecuteTemplate(w, "Index", produtos)
 }
