8d835f7818caee4f0693cbda5ae8f54f54975dd9
Paulo <paulo@MacBook-Pro-de-Desenvolvimento.local>
2022-10-14 18:23:29 -0300

    modularizando templates e função de deletar

--- web/controller/produtos.go
+++ web/controller/produtos.go
@@ -36,3 +36,10 @@ func Insert(w http.ResponseWriter, r *http.Request) {
 	}
 	http.Redirect(w, r, "/", 301)
 }
+
+func Delete(w http.ResponseWriter, r *http.Request) {
+
+	GetId := r.URL.Query().Get("id")
+	models.DeletaProduto(GetId)
+	http.Redirect(w, r, "/", 301)
+}
