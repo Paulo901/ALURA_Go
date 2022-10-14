0f47244576375996afe0cea5beb5cf64d8c427d8
Paulo <paulo@MacBook-Pro-de-Desenvolvimento.local>
2022-10-14 16:48:38 -0300

    novos comportamentos: Adicionar produtos

--- web/controller/produtos.go
+++ web/controller/produtos.go
@@ -1,7 +1,9 @@
 package controller
 
 import (
+	"log"
 	"net/http"
+	"strconv"
 	"text/template"
 	"web/models"
 )
@@ -11,4 +13,26 @@ var temp = template.Must(template.ParseGlob("templates/*.html"))
 func Index(w http.ResponseWriter, r *http.Request) {
 	todosProdutos := models.BuscaProdutos()
 	temp.ExecuteTemplate(w, "Index", todosProdutos)
+}
+
+func New(w http.ResponseWriter, r *http.Request) {
+	temp.ExecuteTemplate(w, "New", nil)
+}
+
+func Insert(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		nome := r.FormValue("nome")
+		preco := r.FormValue("preco")
+		quantidade := r.FormValue("quantidade")
+
+		precoStr, err := strconv.ParseFloat(preco, 64)
+		quantidadeStr, err := strconv.Atoi(quantidade)
+
+		if err != nil {
+			log.Println("Erro de Formatação", err)
+		}
+		models.CriandoProdutos(nome, precoStr, quantidadeStr)
+
+	}
+	http.Redirect(w, r, "/", 301)
 }
