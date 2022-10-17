db4bae95da548dae4f4d81846e0d1b1ae43bdf98
Paulo <paulo@MacBook-Pro-de-Desenvolvimento.local>
2022-10-17 18:09:55 -0300

    metodo atualizar

--- web/models/products.go
+++ web/models/products.go
@@ -64,3 +64,45 @@ func DeletaProduto(id string) {
 
 	defer db.Close()
 }
+
+func EditaProduto(id string) Produto {
+
+	db := db.ConectorBD()
+
+	DadosProduto, err := db.Query("select * from produtos while id=$1" + id)
+	if err != nil {
+		panic(err.Error())
+	}
+	ProdutoAtualizar := Produto{}
+
+	for DadosProduto.Next() {
+
+		var nome string
+		var preco float64
+		var quantidade, id int
+
+		err = DadosProduto.Scan(&id, &nome, &preco, &quantidade)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		ProdutoAtualizar.Id = id
+		ProdutoAtualizar.Nome = nome
+		ProdutoAtualizar.Preco = preco
+		ProdutoAtualizar.Quantidade = quantidade
+
+	}
+	defer db.Close()
+	return ProdutoAtualizar
+}
+
+func AtualizaProduto(id int, nome, descricao string, preco float64, quantidade int) {
+	db := db.ConectorBD()
+
+	AtualizaProduto, err := db.Prepare("update produtos set nome=$1, descricao=$2, preco=$3, quantidade=$4 where id=$5")
+	if err != nil {
+		panic(err.Error())
+	}
+	AtualizaProduto.Exec(nome, descricao, preco, quantidade, id)
+	defer db.Close()
+}
--- web/routes/routes.go
+++ web/routes/routes.go
@@ -10,4 +10,6 @@ func PegaRota() {
 	http.HandleFunc("/new", controller.New)
 	http.HandleFunc("/insert", controller.Insert)
 	http.HandleFunc("/delete", controller.Delete)
+	http.HandleFunc("/edit", controller.Edit)
+	http.HandleFunc("/update", controller.Update)
 }
